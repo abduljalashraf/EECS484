8ad28f1c5b48befed09e87213636de4d673a616b
Anjali Justice <justanj@umich.edu>
2019-11-15 10:16:45 -0500

    query1

--- 484_3/code/query1.js
+++ 484_3/code/query1.js
@@ -5,7 +5,10 @@ function find_user(city, dbname){
     var results = [];
     // TODO: return a Javascript array of user_ids. 
     // db.users.find(...);
-
+    var cur = db.users.find({hometown: {city : city} });
+    while(cur.hasNext){
+        results.push(cur.next());
+    }
     // See test.js for a partial correctness check.  
     // The result will be an array of integers. The order does not matter.                                                               
     return results;
