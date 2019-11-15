26680eb4e2e505130bca146f5618a16a1719e0a3
Anjali Justice <justanj@umich.edu>
2019-11-15 15:15:46 -0500

    query5

--- 484_3/code/query4.js
+++ 484_3/code/query4.js
@@ -20,11 +20,10 @@ function suggest_friends(year_diff, dbname) {
     
     //Find all users that are male loop through results.
     //In each loop find all female users who's YOB is within year_diff of the male user and has the same hometown
-    //For each pair, find if theyre not friends (the smaller id is in the first position of flat_users and the larger id is in second position)
+    //For each pair, find if theyre not friends (userA doesnt have userB in friends array and vice versa)
     //add array of users to final array 
 
     //indexOf returns -1 if not found
-                // if(!db.flat_users.find({"user_id": Math.min(userA.user_id, userB.user_id), "friends": Math.max(userA.user_id, userB.user_id)}).hasNext())
 
     db.users.find({gender: "male"}).forEach(function(userA){
         db.users.find({gender: "female", "hometown.city": userA.hometown.city, YOB: {$lt: userA.YOB + year_diff, $gt: userA.YOB - year_diff}}).forEach(function(userB){
--- 484_3/code/query5.js
+++ 484_3/code/query5.js
@@ -11,5 +11,28 @@ function oldest_friend(dbname){
   var results = {};
   // TODO: implement oldest friends
   // return an javascript object described above
+
+
+  // if((user2.friends.indexOf(user1.user_id) != -1) && user2.age > max)
+  // max = user2.age;
+  // results.user1 
+
+
+  //Get all friends of user1
+  var friends = [];
+  var max = 0;
+  db.users.find().forEach(function(user1){
+    //find all users that have an id lower than user1, add those users to the friends array
+    db.users.find({user_id: {$lt: user1.user_id}}).forEach(function(user2){
+      printjson([user1, user2]);
+    })
+  })
+
+  // friends.forEach(function(friend){
+  //   if(friend.age > max){
+  //     max = friend.age;
+  //   }
+  // })
+
   return results
 }
