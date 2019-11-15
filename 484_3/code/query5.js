870ee89665410a9d5bd0a098710eb4ad7d620dd5
Anjali Justice <justanj@umich.edu>
2019-11-15 15:34:31 -0500

    query5

--- 484_3/code/query5.js
+++ 484_3/code/query5.js
@@ -11,16 +11,7 @@ function oldest_friend(dbname){
   var results = {};
   // TODO: implement oldest friends
   // return an javascript object described above
-
-
-  // if((user2.friends.indexOf(user1.user_id) != -1) && user2.age > max)
-  // max = user2.age;
-  // results.user1 
-
-
-  //Get all friends of user1
   var friends = [];
-  var max = 0;
   db.users.aggregate([
     {$unwind: "$friends"},
     {$project: {
@@ -31,15 +22,12 @@ function oldest_friend(dbname){
     {$group: {
       _id: "$user_id",
       friends: {$push: {user_id:"$friends"}}}},
+    {$out: "user_friends"},
   ]);
 
-
-
-  // friends.forEach(function(friend){
-  //   if(friend.age > max){
-  //     max = friend.age;
-  //   }
-  // })
+  db.user_friends.find().forEach(function(item){
+    printjson(item);
+  })
 
   return results
 }
