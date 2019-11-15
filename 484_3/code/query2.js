ce61f046f61bcf402868c93dc1ba878a538884d9
Anjali Justice <justanj@umich.edu>
2019-11-15 12:34:58 -0500

    query2

--- 484_3/code/query2.js
+++ 484_3/code/query2.js
@@ -11,11 +11,12 @@ function unwind_friends(dbname){
     db = db.getSiblingDB(dbname);
     // TODO: unwind friends
     db.users.aggregate([
+      {$unwind: "$friends"},
       {$project: {
+        _id:0,
         user_id: 1,
         friends: 1,
       }},
-      {$unwind: "$friends"},
       {$out: "flat_users"}
     ]);
     // returns nothing. It creates a collection instead as specified above.
