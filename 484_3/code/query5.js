2a625e208f55cff3582013671d039bdce0fdd6bb
Anjali Justice <justanj@umich.edu>
2019-11-16 10:50:41 -0500

    query5

--- 484_3/code/query5.js
+++ 484_3/code/query5.js
@@ -11,13 +11,37 @@ function oldest_friend(dbname){
   var results = {};
   // TODO: implement oldest friends
   // return an javascript object described above
-  var friends = [];
-  db.flat_users.aggregate([
-    {$group: {
-      _id: "$user_id",
-      friends: {$push: {user_id:"$friends"}}}},
-    {$out: "user_friends"},
-  ]);
+  db.friends.drop();
+  //Make table of all friends by flipping the flat_users table and combining it with the original flat_users table
+	db.runCommand({create: "friends"}, {size: 2 * db.flat_users.find().count()});
+	db.flat_users.find().forEach(function(d) {
+    db.runCommand({insert: "friends", documents: [ {"u1": d.user_id, "u2": d.friends }, 
+    {"u1": d.friends, "u2": d.user_id } ] } );
+  });
+  
+  //put birth years into an object using user_id as the index
+  var yob = {};
+	db.users.find().forEach(function(d) {yob[d.user_id] = d.YOB;});
 
+  db.friends.aggregate([{$group: {_id: "$u1", friend: {$addToSet: "$u2"}}}]).forEach(function(user){
+    //init values for _id, year of birth of the first friend, and the first friend
+    var a = user._id;
+    var maxYear = yob[user.friend[0]];
+    var maxFriend = user.friend[0];
+    //loop through friends and check if friend[i]'s age is larger than the current friend
+    //if it is larger, update the max friend
+    for(i = 0; i < user.friend.length; i++){
+      if(Math.min(maxYear, yob[user.friend[i]]) != y){
+        maxYear = yob[user.friend[i]];
+        maxFriend = user.friend[i];
+      }
+      // else if(Math.max(maxYear, yob[user.friend[i]]) == y){
+      //   //if next friends year is smaller than current friends year, update max friend to be the 
+      //   maxFriend = Math.min(maxFriend, user.friend[i]);
+      // }
+    }
+
+    results[a] = maxFriend;
+  })
   return results
 }
