54d7b8e6e08f3fe5589d447af8d9faef7baddbb7
Anjali Justice <justanj@umich.edu>
2019-11-16 13:20:39 -0500

    query4

--- 484_3/code/query4.js
+++ 484_3/code/query4.js
@@ -26,8 +26,8 @@ function suggest_friends(year_diff, dbname) {
     //flat_users contains all user/friend combos without arrays. The lower id is listed first.
     //Check that the min of userA and userB and the max of userA and userB are not corresponding 
     //entries in the flat_users table. This means that they are not friends
-    db.users.find({gender: "male"}).forEach(function(userA){
-        db.users.find({gender: "female", "hometown.city": userA.hometown.city, YOB: {$lt: userA.YOB + year_diff, $gt: userA.YOB - year_diff}}).forEach(function(userB){            
+    db.users.find({gender: "male", hometown: {$exists: true}}).forEach(function(userA){
+        db.users.find({gender: "female", hometown: {$exists: true}, "hometown.city": userA.hometown.city, YOB: {$lt: userA.YOB + year_diff, $gt: userA.YOB - year_diff}}).forEach(function(userB){            
             if(!db.flat_users.find({"user_id": Math.min(userA.user_id, userB.user_id), "friends": Math.max(userA.user_id, userB.user_id)}).hasNext()) 
             pairs.push([userA.user_id, userB.user_id]);
         })
