627912bb56a56a15fb1c202cb186f8f8f3102c08
Anjali Justice <justanj@umich.edu>
2019-11-16 14:49:58 -0500

    part A

--- 484_3/code/GetData.java
+++ 484_3/code/GetData.java
@@ -111,14 +111,15 @@ public class GetData{
 				// 	temp_user.put("hometown", temp_obj);
 				// }
 				// else{
-					rst2.next();
+					while(rst2.next()){
 					// temp_obj.put("city", rst2.getString(1));
 					// temp_obj.put("state", rst2.getString(2));
 					// temp_obj.put("Country", rst2.getString(3));
 					temp_obj.put("city", "anj");
 					temp_obj.put("state", "is");
 					temp_obj.put("Country", "dumb");
-					temp_user.put("hometown", temp_obj);
+					// temp_user.put("hometown", temp_obj);
+					}
 				// }
 				
     			// add to temp object
@@ -146,7 +147,7 @@ public class GetData{
 					temp_obj2.put("state", "is");
 					temp_obj2.put("Country", "smart");
 
-					temp_user.put("current", temp_obj2);
+					// temp_user.put("current", temp_obj2);
 				// }
 
     			// create a JSONarray for friends (only w/ greater IDs) and add that
