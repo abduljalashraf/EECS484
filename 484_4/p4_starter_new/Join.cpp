9e08e4667fa37f99bfba00a9b880254402ae5475
Anjali Justice <anjalijustice@0587414425.wireless.umich.net>
2019-12-12 13:55:42 -0500

    probe

--- 484_4/p4_starter_new/Join.cpp
+++ 484_4/p4_starter_new/Join.cpp
@@ -172,7 +172,7 @@ vector<unsigned int> probe(Disk* disk, Mem* mem, vector<Bucket>& partitions) {
 
 							// load to output buffer
 							(mem->mem_page((MEM_SIZE_IN_PAGE - 1)))->loadPair(leftRecord, rightRecord);
-							std::cout << "mem added here " << std::endl;
+							std::cout << "mem added here = " << leftRecord << ", " << rightRecord << std::endl;
 							// output_buffer -> loadPair(leftRecord, rightRecord);
 						}
 					}
